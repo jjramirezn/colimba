45601b2320c10b0afc04c34897ed6b5e3e161216
Juan Ramírez <artjjrn@gmail.com>
2023-06-30 00:52:17 -0300

    Improve excersie 1-13
    - Remove unused state
    - Remove limits import
    - Add last word handling to horizontal version
    - Add return 0 on main
    - Add braces everywhere

--- 1/13/hwchist.c
+++ 1/13/hwchist.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 
-#define IN  1
-#define OUT 0
-
-main()
+/* 1-13 Write a program to print a histogram of the lengths of words in its
+ * input. It is easy to draw the histogram with the bars horizontal; a vertical
+ * orientation is more challenging.
+ */
+int main()
 {
-        int c, i, j, nc, state;
+        int c, i, j, nc;
         int nlen[10];
 
-        state = OUT;
         nc = 0;
-        for (i = 0; i < 10; ++i)
+        for (i = 0; i < 10; ++i) {
                 nlen[i] = 0;
+        }
 
         while ((c = getchar()) != EOF) {
                 if (c == ' ' || c == '\n' ||  c == '\t') {
-                        if (nc >= 10)
-                                ++nlen[9];
-                        else
-                                ++nlen[nc-1];
+                        if (nc > 0) {
+                                ++nlen[nc >= 10 ? 9 : nc-1];
+                        }
                         nc = 0;
-                        state = OUT;
                 } else {
                         ++nc;
-                        state = IN;
                 }
+        }
+        /* for the last word */
+        if (nc > 0) {
+                ++nlen[nc >= 10 ? 9 : nc-1];
         }
 
         for (i = 0; i < 9; ++i) {
                 printf("%d   |", i+1);
-                for (j = 0; j < nlen[i]; ++j)
+                for (j = 0; j < nlen[i]; ++j) {
                         putchar('|');
+                }
                 putchar('\n');
         }
         printf("+10 |");
-        for (j = 0; j < nlen[9]; ++j)
+        for (j = 0; j < nlen[9]; ++j) {
                 putchar('|');
+        }
         putchar('\n');
+        return 0;
 }
--- 1/13/vwchist.c
+++ 1/13/vwchist.c
@@ -1,60 +1,60 @@
-#include <limits.h>
 #include <stdio.h>
 
-#define IN  1
-#define OUT 0
+#define HEIGHT 20
 
-main()
+/* 1-13 Write a program to print a histogram of the lengths of words in its
+ * input. It is easy to draw the histogram with the bars horizontal; a vertical
+ * orientation is more challenging.
+ */
+int main()
 {
-        int c, i, j, nc, state, max;
+        int c, i, j, nc, max;
         int nlen[10];
 
-        state = OUT;
         nc = 0;
-        for (i = 0; i < 10; ++i)
+        for (i = 0; i < 10; ++i) {
                 nlen[i] = 0;
+        }
 
         while ((c = getchar()) != EOF) {
                 if (c == ' ' || c == '\n' ||  c == '\t') {
-                        if (nc >= 10)
-                                ++nlen[9];
-                        else
-                                ++nlen[nc-1];
+                        if (nc > 0) {
+                                ++nlen[nc >= 10 ? 9 : nc-1];
+                        }
                         nc = 0;
-                        state = OUT;
                 } else {
                         ++nc;
-                        state = IN;
                 }
         }
         /* for the last word */
-        if (nc >= 10)
-                ++nlen[9];
-        else if (nc > 0)
-                ++nlen[nc-1];
+        if (nc > 0) {
+                ++nlen[nc >= 10 ? 9 : nc-1];
+        }
 
-        max = INT_MIN;
-        for (i = 0; i < 10; ++i)
-                if (nlen[i] > max)
+        max = nlen[0];
+        for (i = 1; i < 10; ++i) {
+                if (nlen[i] > max) {
                         max = nlen[i];
-        if (max > 0)
-                for (i = 0; i < 10; ++i)
-                        nlen[i] = nlen[i] * 20 / max;          /* normalize */
+                }
+        }
+        if (max > 0) {
+                for (i = 0; i < 10; ++i) {
+                        nlen[i] = nlen[i] * HEIGHT / max;       /* normalize */
+                }
+        }
 
 
         printf("|------------------------------|\n");
-        for (i = 20; i >= 0; --i) {
+        for (i = HEIGHT; i >= 0; --i) {
                 putchar('|');
                 for (j = 0; j < 10; ++j) {
-                        if (nlen[j] >= i)
-                                printf("===");
-                        else
-                                printf("   ");
+                        printf(nlen[j] >= i ? "===" : "   ");
                 }
                 printf("|\n");
         }
         printf("|------------------------------|\n");
         printf("| 1  2  3  4  5  6  7  8  9 +10|\n");
         printf("|------------------------------|\n");
+        return 0;
 }
 
